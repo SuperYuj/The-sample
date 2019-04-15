8f95895ab1589f0f3746616a6172aaefedfb7688
绿巨人 <47655275+SuperYuj@users.noreply.github.com>
2019-04-15 16:51:16 +0800

    Add files via upload

---
+++ cookie.session.flash.ejs.错误信息.promise封装合集和图片上传.ajax - 重构 - 副本/utils/querydb.js
@@ -0,0 +1,17 @@
+// promise的异步封装
+var db = require('../config/db');
+
+var querydb = function (sql, parameters) {
+
+    return new Promise(function (resolve, rejected) {
+        db.query(sql, parameters, function (err, data) {
+            if (err) {
+                rejected(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+};
+
+module.exports = querydb;
